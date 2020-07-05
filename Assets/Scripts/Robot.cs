e5b7cdee219797f0e5b0d5f732d79e57ce974718
Kyeore Heo <91kyoheo@gmail.com>
2020-07-05 01:24:23 -0400

    update robot name based on their position

---
+++ Assets/Scripts/Robot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Robot : MonoBehaviour
+{
+    private List<string> directions = new List<string> { "front", "left", "back", "right" };
+    public Vector3 currentPosition;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        currentPosition = this.transform.position;
+        this.gameObject.name = "RB(" + currentPosition.x.ToString() + ", " + currentPosition.z.ToString() + ")";
+    }
+}
