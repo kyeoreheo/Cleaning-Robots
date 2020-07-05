1185a913ee0c3d3b64441b1c21639054467d6c85
Kyeore Heo <91kyoheo@gmail.com>
2020-07-05 00:57:16 -0400

    Define Board, Robots

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+
+    public int boardSize;
+    public Transform block;
+    public Material whiteMaterial;
+    public Material blackMaterial;
+
+    private bool isWhite = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < boardSize; x++)
+        {
+            isWhite = !isWhite;
+            for (int z = 0; z < boardSize; z++)
+            {
+                Material currentMaterial;
+                if (isWhite)
+                {
+                    currentMaterial = whiteMaterial;
+                }
+                else
+                {
+                    currentMaterial = blackMaterial;
+                }
+                Transform copidBlock = Instantiate(block, new Vector3(x, 0, z), Quaternion.identity);
+                copidBlock.parent = this.gameObject.transform;
+                copidBlock.gameObject.GetComponent<MeshRenderer>().material = currentMaterial;
+                isWhite = !isWhite;
+                copidBlock.gameObject.name = "(" + x.ToString() + ", " + z.ToString() + ")";
+            }
+
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RobotManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotManager : MonoBehaviour
+{
+    public int robotNumber;
+    public int boardSize;
+    public Transform robot;
+
+    private int robotCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(Random.RandomRange(0, boardSize));
+        for (int x = 0; x < boardSize; x++)
+        {
+            for (int z = 0; z < boardSize; z++)
+            {
+                if (robotCount < robotNumber)
+                {
+                    Transform copidRobot = Instantiate(robot, new Vector3(Random.RandomRange(0, boardSize), 1.5f, z), Quaternion.identity);
+                    copidRobot.parent = this.gameObject.transform;
+                    copidRobot.gameObject.name = "RB(" + x.ToString() + ", " + z.ToString() + ")";
+                    robotCount++;
+
+                }
+
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
