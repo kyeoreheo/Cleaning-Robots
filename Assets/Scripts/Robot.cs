ff355b474c629164cf89c3eafaa3374ed169e000
Kyeore Heo <91kyoheo@gmail.com>
2020-07-05 12:26:18 -0400

    move with random direction for 1 block

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,20 +1,150 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+//using System;
 
 public class Robot : MonoBehaviour
 {
-    private List<string> directions = new List<string> { "front", "left", "back", "right" };
     public Vector3 currentPosition;
+    public float moveOnEverySecond;
+
+    private List<string> directions = new List<string> { "front", "left", "back", "right" };
+    private bool shouldMove = true;
+    private Vector3 destinationBlock;
+
+    public float currentTimer = 0;
+    public string direction;
     void Start()
     {
-
+        currentPosition = this.gameObject.transform.position;
+        GenerateRandomDirection();
+        RotateRobot();
     }
 
     void Update()
     {
-        currentPosition = this.transform.position;
-        this.gameObject.name = "RB(" + currentPosition.x.ToString() + ", " + currentPosition.z.ToString() + ")";
+        this.gameObject.name = "RB(" + this.transform.position.x.ToString() + ", " + this.transform.position.z.ToString() + ")";
+        Timer();
+        MoveRandom();
+        if (currentTimer == 0)
+        {
+            shouldMove = true;
+            GenerateRandomDirection();
+            RotateRobot();
+        }
     }
+
+    private void Timer()
+    {
+        currentTimer += Time.deltaTime;
+        if (currentTimer >= moveOnEverySecond)
+        {
+            currentTimer = 0;
+        }
+    }
+
+    private void GenerateRandomDirection()
+    {
+        int randomIndex = Random.RandomRange(0, 4);
+        direction = directions[randomIndex];
+    }
+
+    private void RotateRobot()
+    {
+        if (direction == "fornt")
+        {
+            this.transform.rotation = Quaternion.AngleAxis(180.0f, Vector3.up);
+
+        }
+        else if (direction == "left")
+        {
+            this.transform.rotation = Quaternion.AngleAxis(-90.0f, Vector3.up);
+        }
+        else if (direction == "back")
+        {
+            this.transform.rotation = Quaternion.AngleAxis(-180.0f, Vector3.up);
+        }
+        else if (direction == "right")
+        {
+            this.transform.rotation = Quaternion.AngleAxis(90.0f, Vector3.up);
+        }
+
+    }
+
+    public void MoveRandom()
+    {
+        if (direction == "fornt")
+        {
+            MoveForward();
+        }
+        else if (direction == "left")
+        {
+            MoveLeft();
+        }
+        else if (direction == "back")
+        {
+            MoveBack();
+        }
+        else if (direction == "right")
+        {
+            MoveRight();
+        }
+    }
+
+    private void MoveForward()
+    {
+        destinationBlock = new Vector3(currentPosition.x, 1.5f, currentPosition.z + 1);
+        if (this.transform.position.z <= destinationBlock.z && shouldMove)
+        {
+            this.transform.position += Vector3.forward * Time.deltaTime;
+        }
+        else
+        {
+            shouldMove = false;
+            currentPosition = this.gameObject.transform.position;
+        }
+    }
+
+    private void MoveLeft()
+    {
+        destinationBlock = new Vector3(currentPosition.x - 1, 1.5f, currentPosition.z);
+        if (this.transform.position.x >= destinationBlock.x && shouldMove)
+        {
+            this.transform.position += Vector3.left * Time.deltaTime;
+        }
+        else
+        {
+            shouldMove = false;
+            currentPosition = this.gameObject.transform.position;
+        }
+    }
+
+    private void MoveBack()
+    {
+        destinationBlock = new Vector3(currentPosition.x, 1.5f, currentPosition.z - 1);
+        if (this.transform.position.z >= destinationBlock.z && shouldMove)
+        {
+            this.transform.position += Vector3.back * Time.deltaTime;
+        }
+        else
+        {
+            shouldMove = false;
+            currentPosition = this.gameObject.transform.position;
+        }
+    }
+
+    private void MoveRight()
+    {
+        destinationBlock = new Vector3(currentPosition.x + 1, 1.5f, currentPosition.z);
+        if (this.transform.position.x <= destinationBlock.x && shouldMove)
+        {
+            this.transform.position += Vector3.right * Time.deltaTime;
+        }
+        else
+        {
+            shouldMove = false;
+            currentPosition = this.gameObject.transform.position;
+        }
+    }
+
 }
